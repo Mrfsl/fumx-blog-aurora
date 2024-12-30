37bd091dbfbc3627ad0b5254b9310ff112bec007
fmx <2569864603@qq.com>
2024-12-30 13:48:41 +0800

    1

--- aurora-vue/aurora-blog/src/stores/meta.ts
+++ aurora-vue/aurora-blog/src/stores/meta.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useMetaStore = defineStore('metaStore', {
   state: () => {
     return {
-      title: '墨染的个人博客'      // 网页的title
+      title: '墨染的开发之旅'      // 网页的title
     }
   }
 })
