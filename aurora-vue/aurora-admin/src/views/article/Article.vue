c12800ba4b032ae1058c6772b94c37572385a483
fmx <2569864603@qq.com>
2025-01-06 11:58:21 +0800

    1

--- aurora-vue/aurora-admin/src/views/article/Article.vue
+++ aurora-vue/aurora-admin/src/views/article/Article.vue
@@ -222,7 +222,7 @@ export default {
       this.addOrEdit = true
     },
     uploadCover(response) {
-      this.article.articleCover = response.data      // 后端删掉了一层，预览添加上
+      this.article.articleCover = response.data
     },
     beforeUpload(file) {
       return new Promise((resolve) => {
